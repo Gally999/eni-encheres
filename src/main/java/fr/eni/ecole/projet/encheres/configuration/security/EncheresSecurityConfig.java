4d20ad2c8a7cf305ff81b2946c29c84becbffceb
Gally <gally999@free.fr>
2025-03-03 16:19:16 +0100

    add login form + intl message files

--- src/main/java/fr/eni/ecole/projet/encheres/configuration/security/EncheresSecurityConfig.java
+++ src/main/java/fr/eni/ecole/projet/encheres/configuration/security/EncheresSecurityConfig.java
@@ -1,35 +1,49 @@
 package fr.eni.ecole.projet.encheres.configuration.security;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class EncheresSecurityConfig {
-	
+
+	@Bean
+	UserDetailsManager userDetailsManager(DataSource dataSource) {
+		JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
+		userDetailsManager.setUsersByUsernameQuery("SELECT pseudo, mot_de_passe, 1 FROM Utilisateurs WHERE pseudo = ?;");
+		userDetailsManager.setAuthoritiesByUsernameQuery("SELECT pseudo, role FROM Utilisateurs u INNER JOIN Roles r ON u.administrateur = r.is_admin WHERE pseudo = ?;");
+		return userDetailsManager;
+	}
+
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests(auth -> {
-			
-			auth	
-				//permettre à tout le monde d'accéder à l'URL racine
-				.requestMatchers("/*").permitAll()
-				
-				// Permettre à tous les utilisateurs d'afficher correctement les images et la css
-				.requestMatchers("/css/*").permitAll()
-				.requestMatchers("/images/*").permitAll()
-				
-				// Toutes autres url et méthodes HTTP ne sont pas permises
-				.anyRequest().denyAll();
+
+			auth
+					// permettre à tout le monde d'accéder à l'URL racine
+					.requestMatchers("/*").permitAll()
+
+					// Permettre à tous les utilisateurs d'afficher correctement les images et la
+					// css
+					.requestMatchers("/css/*").permitAll().requestMatchers("/images/*").permitAll()
+
+					// Toutes autres url et méthodes HTTP ne sont pas permises
+					.anyRequest().denyAll();
 		});
-		
-	    // Customiser le formulaire de login
-		http.formLogin(Customizer.withDefaults());
-		
+
+		// Customiser le formulaire de login
+		http.formLogin(form -> {
+			form.loginPage("/login").permitAll();
+			form.defaultSuccessUrl("/");
+		});
+
 		return http.build();
 	}
 
---
+++ src/main/java/fr/eni/ecole/projet/encheres/controller/security/LoginController.java
@@ -0,0 +1,14 @@
+package fr.eni.ecole.projet.encheres.controller.security;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+	
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+
+}
