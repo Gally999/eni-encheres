c14ad34977501aeb3f09da5e0a20b69617686e7e
Gally <gally999@free.fr>
2025-03-04 11:33:50 +0100

    upd encheres controller

--- src/main/java/fr/eni/ecole/projet/encheres/controller/EncheresController.java
+++ src/main/java/fr/eni/ecole/projet/encheres/controller/EncheresController.java
@@ -1,10 +1,37 @@
 package fr.eni.ecole.projet.encheres.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import fr.eni.ecole.projet.encheres.bll.EncheresService;
+import fr.eni.ecole.projet.encheres.bo.ArticleAVendre;
 
 @Controller
 public class EncheresController {
-	public void welcome() {
-		System.out.println ("Test");
+	
+	private EncheresService encheresService;
+	
+	
+	public EncheresController(EncheresService encheresService) {
+		this.encheresService = encheresService;
 	}
+
+
+	public void welcome() {
+		System.out.println ("Test Welcome");
+	}
+	
+	@GetMapping("/")
+	public String afficherEncheresActives(Model model) {
+		// Récupérer les enchères actives de la BLL
+		List<ArticleAVendre> encheresActives = encheresService.consulterEncheresActives();
+		// Ajout des enchères actives dans le model
+		model.addAttribute("encheresActives", encheresActives);
+		
+		return "view-encheres";
+	}
+	
 }
