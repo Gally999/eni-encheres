1ee181c4190dc2d2f52f89c66517fb2c14ef12c8
Gally <gally999@free.fr>
2025-03-04 11:10:42 +0100

    add encheres service

--- src/main/java/fr/eni/ecole/projet/encheres/bo/Utilisateur.java
+++ src/main/java/fr/eni/ecole/projet/encheres/bo/Utilisateur.java
@@ -4,7 +4,7 @@ package fr.eni.ecole.projet.encheres.bo;
 import java.util.Objects;
 
 public class Utilisateur {
-
+	
 	private String pseudo;
 	private String nom;
 	private String prenom;
@@ -15,17 +15,11 @@ public class Utilisateur {
 	private boolean admin;
 	private Adresse adresse;
 	
-	
-	
 	public Utilisateur() {
-		super();
 	}
-
-
 
 	public Utilisateur(String pseudo, String nom, String prenom, String email, String telephone, String motDePasse,
 			int credit, boolean admin, Adresse adresse) {
-		super();
 		this.pseudo = pseudo;
 		this.nom = nom;
 		this.prenom = prenom;
@@ -36,116 +30,78 @@ public class Utilisateur {
 		this.admin = admin;
 		this.adresse = adresse;
 	}
-
-
 
 	public Adresse getAdresse() {
 		return adresse;
 	}
 
-
-
 	public void setAdresse(Adresse adresse) {
 		this.adresse = adresse;
 	}
-
-
 
 	public String getPseudo() {
 		return pseudo;
 	}
 
-
-
 	public void setPseudo(String pseudo) {
 		this.pseudo = pseudo;
 	}
-
-
 
 	public String getNom() {
 		return nom;
 	}
 
-
-
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
-
-
 
 	public String getPrenom() {
 		return prenom;
 	}
 
-
-
 	public void setPrenom(String prenom) {
 		this.prenom = prenom;
 	}
-
-
 
 	public String getEmail() {
 		return email;
 	}
 
-
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
-
 
 	public String getTelephone() {
 		return telephone;
 	}
 
-
-
 	public void setTelephone(String telephone) {
 		this.telephone = telephone;
 	}
-
-
 
 	public String getMotDePasse() {
 		return motDePasse;
 	}
 
-
-
 	public void setMotDePasse(String motDePasse) {
 		this.motDePasse = motDePasse;
 	}
-
-
 
 	public int getCredit() {
 		return credit;
 	}
 
-
-
 	public void setCredit(int credit) {
 		this.credit = credit;
 	}
-
-
 
 	public boolean isAdmin() {
 		return admin;
 	}
 
-
-
 	public void setAdmin(boolean admin) {
 		this.admin = admin;
 	}
-
-
 
 	@Override
 	public String toString() {
@@ -172,14 +128,10 @@ public class Utilisateur {
 		return builder.toString();
 	}
 
-
-
 	@Override
 	public int hashCode() {
 		return Objects.hash(email, nom, prenom, pseudo);
 	}
-
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -192,8 +144,6 @@ public class Utilisateur {
 		Utilisateur other = (Utilisateur) obj;
 		return Objects.equals(email, other.email) && Objects.equals(nom, other.nom)
 				&& Objects.equals(prenom, other.prenom) && Objects.equals(pseudo, other.pseudo);
-	}
-
-		
+	}	
 		
 }
