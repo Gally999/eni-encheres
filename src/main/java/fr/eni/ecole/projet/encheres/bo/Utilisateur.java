f5295b5a4f01940937a0d0102fc399587a969557
PETIT Laetitia <laetitia.petit2024@campus-eni.fr>
2025-03-04 10:45:22 +0100

    Utilisateur BO + message

--- src/main/java/fr/eni/ecole/projet/encheres/bo/Utilisateur.java
+++ src/main/java/fr/eni/ecole/projet/encheres/bo/Utilisateur.java
@@ -1,5 +1,199 @@
+
 package fr.eni.ecole.projet.encheres.bo;
+
+import java.util.Objects;
 
 public class Utilisateur {
 
+	private String pseudo;
+	private String nom;
+	private String prenom;
+	private String email;
+	private String telephone;
+	private String motDePasse;
+	private int credit;
+	private boolean admin;
+	private Adresse adresse;
+	
+	
+	
+	public Utilisateur() {
+		super();
+	}
+
+
+
+	public Utilisateur(String pseudo, String nom, String prenom, String email, String telephone, String motDePasse,
+			int credit, boolean admin, Adresse adresse) {
+		super();
+		this.pseudo = pseudo;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.email = email;
+		this.telephone = telephone;
+		this.motDePasse = motDePasse;
+		this.credit = credit;
+		this.admin = admin;
+		this.adresse = adresse;
+	}
+
+
+
+	public Adresse getAdresse() {
+		return adresse;
+	}
+
+
+
+	public void setAdresse(Adresse adresse) {
+		this.adresse = adresse;
+	}
+
+
+
+	public String getPseudo() {
+		return pseudo;
+	}
+
+
+
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
+
+
+
+	public String getNom() {
+		return nom;
+	}
+
+
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+
+
+	public String getMotDePasse() {
+		return motDePasse;
+	}
+
+
+
+	public void setMotDePasse(String motDePasse) {
+		this.motDePasse = motDePasse;
+	}
+
+
+
+	public int getCredit() {
+		return credit;
+	}
+
+
+
+	public void setCredit(int credit) {
+		this.credit = credit;
+	}
+
+
+
+	public boolean isAdmin() {
+		return admin;
+	}
+
+
+
+	public void setAdmin(boolean admin) {
+		this.admin = admin;
+	}
+
+
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Utilisateur [pseudo=");
+		builder.append(pseudo);
+		builder.append(", nom=");
+		builder.append(nom);
+		builder.append(", prenom=");
+		builder.append(prenom);
+		builder.append(", email=");
+		builder.append(email);
+		builder.append(", telephone=");
+		builder.append(telephone);
+		builder.append(", motDePasse=");
+		builder.append(motDePasse);
+		builder.append(", credit=");
+		builder.append(credit);
+		builder.append(", admin=");
+		builder.append(admin);
+		builder.append(", adresse=");
+		builder.append(adresse);
+		builder.append("]");
+		return builder.toString();
+	}
+
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(email, nom, prenom, pseudo);
+	}
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Utilisateur other = (Utilisateur) obj;
+		return Objects.equals(email, other.email) && Objects.equals(nom, other.nom)
+				&& Objects.equals(prenom, other.prenom) && Objects.equals(pseudo, other.pseudo);
+	}
+
+		
+		
 }
