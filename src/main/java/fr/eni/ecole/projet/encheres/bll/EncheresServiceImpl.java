983d425c370efe7d85393f18bbe65b6f762a4740
Gally <gally999@free.fr>
2025-03-04 11:20:56 +0100

    add Encheres Service Impl

---
+++ src/main/java/fr/eni/ecole/projet/encheres/bll/EncheresServiceImpl.java
@@ -0,0 +1,29 @@
+package fr.eni.ecole.projet.encheres.bll;
+
+import java.util.List;
+
+import fr.eni.ecole.projet.encheres.bo.ArticleAVendre;
+import fr.eni.ecole.projet.encheres.dal.ArticleAVendreDAO;
+
+public class EncheresServiceImpl implements EncheresService {
+	
+	private ArticleAVendreDAO articleDAO;
+	
+	
+	public EncheresServiceImpl(ArticleAVendreDAO articleDAO) {
+		this.articleDAO = articleDAO;
+	}
+
+	@Override
+	public List<ArticleAVendre> consulterEncheres() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ArticleAVendre> consulterEncheresActives() {
+		
+		return articleDAO.findAllActive();
+	}
+
+}
