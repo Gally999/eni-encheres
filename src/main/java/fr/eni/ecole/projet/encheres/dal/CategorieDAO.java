c4f6cb2b29a6ee730ba86d20c38454ae3a39d82b
Gally <gally999@free.fr>
2025-03-05 15:59:58 +0100

    add CategorieDAO + Impl

---
+++ src/main/java/fr/eni/ecole/projet/encheres/dal/CategorieDAO.java
@@ -0,0 +1,9 @@
+package fr.eni.ecole.projet.encheres.dal;
+
+import fr.eni.ecole.projet.encheres.bo.Categorie;
+
+import java.util.List;
+
+public interface CategorieDAO {
+  List<Categorie> findAll();
+}
