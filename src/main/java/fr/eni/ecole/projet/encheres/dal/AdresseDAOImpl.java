0756ce7143a6b1464c6aba7874a7163c5b968e57
Gally <gally999@free.fr>
2025-03-06 15:20:50 +0100

    Add rowMappers to Cat + Adresse + read(id)

--- src/main/java/fr/eni/ecole/projet/encheres/dal/AdresseDAOImpl.java
+++ src/main/java/fr/eni/ecole/projet/encheres/dal/AdresseDAOImpl.java
@@ -1,20 +1,21 @@
 package fr.eni.ecole.projet.encheres.dal;
 
 import fr.eni.ecole.projet.encheres.bo.Adresse;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 @Repository
 public class AdresseDAOImpl implements AdresseDAO {
 
-  // TODO add the one from user
   public static final String FIND_ALL_AVAILABLE = "SELECT no_adresse, rue, code_postal, ville FROM Adresses " +
       "WHERE adresse_eni = 1 OR no_adresse = :id;";
+  public static final String FIND_BY_ID = "SELECT no_adresse, rue, code_postal, ville FROM Adresses WHERE no_adresse = :adresseId;";
 
   private final NamedParameterJdbcTemplate jdbcTemplate;
 
@@ -26,6 +27,25 @@ public class AdresseDAOImpl implements AdresseDAO {
   public List<Adresse> findAllAvailable(long idAdresseUser) {
     MapSqlParameterSource namedParam = new MapSqlParameterSource();
     namedParam.addValue("id", idAdresseUser);
-    return jdbcTemplate.query(FIND_ALL_AVAILABLE, namedParam, new BeanPropertyRowMapper<>(Adresse.class));
+    return jdbcTemplate.query(FIND_ALL_AVAILABLE, namedParam, new AdresseRowMapper());
+  }
+
+  @Override
+  public Adresse read(long id) {
+    MapSqlParameterSource namedParam = new MapSqlParameterSource();
+    namedParam.addValue("adresseId", id);
+    return jdbcTemplate.queryForObject(FIND_BY_ID, namedParam, new AdresseRowMapper());
+  }
+
+  static class AdresseRowMapper implements RowMapper<Adresse> {
+    @Override
+    public Adresse mapRow(ResultSet rs, int rowNum) throws SQLException {
+      Adresse a = new Adresse();
+      a.setId(rs.getLong("no_adresse"));
+      a.setRue(rs.getString("rue"));
+      a.setCodePostal(rs.getString("code_postal"));
+      a.setVille(rs.getString("ville"));
+      return a;
+    }
   }
 }
--- src/main/java/fr/eni/ecole/projet/encheres/dal/CategorieDAOImpl.java
+++ src/main/java/fr/eni/ecole/projet/encheres/dal/CategorieDAOImpl.java
@@ -1,25 +1,45 @@
 package fr.eni.ecole.projet.encheres.dal;
 
 import fr.eni.ecole.projet.encheres.bo.Categorie;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 @Repository
 public class CategorieDAOImpl implements CategorieDAO {
 
-  public static final String FIND_ALL = "SELECT no_categorie, libelle FROM Categories";
+  public static final String FIND_ALL = "SELECT no_categorie, libelle FROM Categories;";
+  public static final String FIND_BY_ID = "SELECT no_categorie, libelle FROM Categories WHERE no_categorie = :categorieId;";
+  private final NamedParameterJdbcTemplate jdbcTemplate;
 
-  private final JdbcTemplate jdbcTemplate;
-
-  public CategorieDAOImpl(JdbcTemplate jdbcTemplate) {
+  public CategorieDAOImpl(NamedParameterJdbcTemplate jdbcTemplate) {
     this.jdbcTemplate = jdbcTemplate;
   }
 
   @Override
   public List<Categorie> findAll() {
-    return jdbcTemplate.query(FIND_ALL, new BeanPropertyRowMapper<>(Categorie.class));
+    return jdbcTemplate.query(FIND_ALL, new CategorieRowMapper());
+  }
+
+  @Override
+  public Categorie read(long id) {
+    MapSqlParameterSource namedParam = new MapSqlParameterSource();
+    namedParam.addValue("categorieId", id);
+    return jdbcTemplate.queryForObject(FIND_BY_ID, namedParam, new CategorieRowMapper());
+  }
+
+  static class CategorieRowMapper implements RowMapper<Categorie> {
+    @Override
+    public Categorie mapRow(ResultSet rs, int rowNum) throws SQLException {
+      Categorie c = new Categorie();
+      c.setId(rs.getLong("no_categorie"));
+      c.setLibelle(rs.getString("libelle"));
+      return c;
+    }
   }
 }
